d09cae371b42691568cce02e9b193cb62e1558fb
ming <2387604631@qq.com>
2020-11-20 09:41:13 +0800

    温度-pm图样式修改

--- static/dist/js/PTH_PM.js
+++ static/dist/js/PTH_PM.js
@@ -2,7 +2,10 @@ function PTH_PM_1(data) {
     var myChart1 = echarts.init(document.getElementById("PTH_PM_1"));
 option = {
     legend: {
-        data: ['pm10','pm2.5']
+        data: ['pm10','pm2.5'],
+        textStyle: {
+                color: '#ffffff'
+            }
     },
     tooltip: {
         trigger: 'axis',
@@ -19,6 +22,18 @@ option = {
         type: 'value',
         axisLabel: {
             formatter: '{value} μg/m³'
+        },
+        axisLabel: {
+            show: true,
+            textStyle: {
+                color: '#ffffff'
+            }
+        },
+        axisLine:{
+            lineStyle:{
+                color:'#ffffff',
+
+            }
         }
     },
     xAxis: {
@@ -28,7 +43,19 @@ option = {
             formatter: '{value}℃ '
         },
         boundaryGap: false,
-        data: data.temperature
+        data: data.temperature,
+        axisLabel: {
+            show: true,
+            textStyle: {
+                color: '#ffffff'
+            }
+        },
+        axisLine:{
+            lineStyle:{
+                color:'#ffffff',
+
+            }
+        }
     },
     series: [
         {
@@ -68,13 +95,14 @@ function PTH_PM_2(data) {
 option = {
     title: {
         text: '湿度与PM',
-        subtext: ' '
+        subtext: ' ',
+        textStyle: {
+                color: '#ffffff'
+        }
     },
     tooltip: {
         trigger: 'axis',
-        axisPointer: {
-            type: 'cross'
-        }
+
     },
     toolbox: {
         show: true,
@@ -85,39 +113,42 @@ option = {
     xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: data.humidity
+        data: data.humidity,
+        axisLabel: {
+            show: true,
+            textStyle: {
+                color: '#ffffff'
+            }
+        },
+        axisLine:{
+            lineStyle:{
+                color:'#ffffff',
+
+            }
+        }
     },
     yAxis: {
         type: 'value',
         axisLabel: {
             formatter: '{value} μg/m3'
         },
-        axisPointer: {
-            snap: true
+        axisLabel: {
+            show: true,
+            textStyle: {
+                color: '#ffffff'
+            }
+        },
+        axisLine:{
+            lineStyle:{
+                color:'#ffffff',
+
+            }
         }
     },
     visualMap: {
         show: false,
         dimension: 0,
-        pieces: [{
-            lte: 6,
-            color: 'green'
-        }, {
-            gt: 6,
-            lte: 8,
-            color: 'red'
-        }, {
-            gt: 8,
-            lte: 14,
-            color: 'green'
-        }, {
-            gt: 14,
-            lte: 17,
-            color: 'red'
-        }, {
-            gt: 17,
-            color: 'green'
-        }]
+
     },
     series: [
         {
@@ -125,38 +156,22 @@ option = {
             type: 'line',
             smooth: true,
             data: data.p1,
-            markArea: {
-                data: [ [{
-                    name: '峰值',
-                    xAxis: '07:30'
-                }, {
-                    xAxis: '10:00'
-                }], [{
-                    name: '晚高峰',
-                    xAxis: '17:30'
-                }, {
-                    xAxis: '21:15'
-                }] ]
+            lineStyle:{
+                color:'#ff0002',
+
             }
+
         },
         {
             name: 'pm2.5',
             type: 'line',
             smooth: true,
             data: data.p2,
-            markArea: {
-                data: [ [{
-                    name: '峰值',
-                    xAxis: '10:00'
-                }, {
-                    xAxis: '12:30'
-                }], [{
-                    name: '峰值',
-                    xAxis: '17:30'
-                }, {
-                    xAxis: '21:15'
-                }] ]
+            lineStyle:{
+                color:'#fdff05',
+
             }
+
         }
     ]
 };
