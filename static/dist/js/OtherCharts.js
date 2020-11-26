bd364a9e78f15b22acfe0c1ee46182f9f38346f1
lxx <lxx981008@163.com>
2020-11-26 14:39:26 +0800

    页面框架布局修改

--- static/dist/js/OtherCharts.js
+++ static/dist/js/OtherCharts.js
@@ -183,3 +183,225 @@ function realTemAndHum_data() {
     myChart.setOption(option);
     
 }
+
+// 传感器比例饼图
+function proportion(datax,datay) {
+
+    var myChart = echarts.init(document.getElementById("sensor_proportion_1"));
+    option = {
+    title: {
+        text: '传感器各类型占比',
+        subtext: '',
+        left: 'center',
+        textStyle: {
+            color: '#ffffff'
+        }
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: datax,
+        textStyle: {
+            color: '#ffffff'
+        }
+    },
+    series: [
+        {
+            name: '数量',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data:datay,
+            emphasis: {
+                itemStyle: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+};
+    myChart.setOption(option);
+
+}
+
+
+function P_TH_0(data) {
+    var myChart = echarts.init(document.getElementById("P_TH_0"));
+var colors = ['#5793f3', '#d14a61', '#675bba'];
+
+
+option = {
+    color: colors,
+    title: {
+        text: '温湿度-压力',
+        textStyle: {
+                color: '#ffffff'
+        }
+    },
+    tooltip: {
+        trigger: 'none',
+        axisPointer: {
+            type: 'cross'
+        }
+    },
+    legend: {
+        data:['温度', '湿度'],
+        left:'right',
+        textStyle: {
+                color: '#ffffff'
+        }
+
+    },
+    grid: {
+        top: 70,
+        bottom: 50
+    },
+    xAxis: [
+        {
+            type: 'category',
+            axisTick: {
+                alignWithLabel: true
+            },
+            axisLine: {
+                onZero: false,
+                lineStyle: {
+                    color: colors[1]
+                }
+            },
+            axisPointer: {
+                label: {
+                    formatter: function (params) {
+                        return '温度：  ' + params.value
+                            + (params.seriesData.length ? '：' + params.seriesData[0].data : '');
+                    }
+                }
+            },
+            data: data.pressure
+        },
+        {
+            type: 'category',
+            axisTick: {
+                alignWithLabel: true
+            },
+            axisLine: {
+                onZero: false,
+                lineStyle: {
+                    color: colors[0]
+                }
+            },
+            axisPointer: {
+                label: {
+                    formatter: function (params) {
+                        return '湿度：  ' + params.value
+                            + (params.seriesData.length ? '：' + params.seriesData[0].data : '');
+                    }
+                }
+            },
+            data: data.pressure
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value',
+            axisLabel: {
+            show: true,
+            textStyle: {
+                color: '#9287e7',
+            }
+        },
+        axisLine:{
+            lineStyle:{
+                color:'#9287e7',
+
+            }
+        }
+        }
+    ],
+    series: [
+        {
+            name: '温度',
+            type: 'line',
+            xAxisIndex: 1,
+            smooth: true,
+            data: data.p1,
+        },
+        {
+            name: '湿度',
+            type: 'line',
+            smooth: true,
+            data: data.p2
+        }
+    ]
+};
+
+    myChart.setOption(option);
+
+}
+//湿度/压力
+function P_TH_1() {
+    var myChart = echarts.init(document.getElementById("P_TH_1"));
+    option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'line',
+            symbol: 'triangle',
+            symbolSize: 20,
+            lineStyle: {
+                color: 'green',
+                width: 4,
+                type: 'dashed'
+            },
+            itemStyle: {
+                borderWidth: 3,
+                borderColor: 'yellow',
+                color: 'blue'
+            }
+        }]
+    };
+    myChart.setOption(option);
+}
+
+//湿度/PM
+function PTH_PM_1() {
+    var myChart = echarts.init(document.getElementById("PTH_PM_1"));
+    option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'line',
+            symbol: 'triangle',
+            symbolSize: 20,
+            lineStyle: {
+                color: 'green',
+                width: 4,
+                type: 'dashed'
+            },
+            itemStyle: {
+                borderWidth: 3,
+                borderColor: 'yellow',
+                color: 'blue'
+            }
+        }]
+    };
+    myChart.setOption(option);
+
+}
